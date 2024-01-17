3bfd53761b42caf085d5b0a42065a81d4c91a2da
OswaldIR <os.ir.8897@gmail.com>
2024-01-17 14:14:38 -0600

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ const Header = () => {
   return (
     <header className='p-2.5 flex w-full items-center justify-between fixed top-0 z-50 bg-white'>
       <nav className='flex-grow basis-0  mx-4'>
-        <a href="/"><img className='max-h-[60px]' src="./img/logo.png" alt="Logo" /></a>
+        <a href="/"><img className='max-h-[60px] max-w-[195px]' src="./img/logo.png" alt="Logo" /></a>
       </nav>
-      <nav>
+      <nav className=''>
         <ul className='font-montserrat font-bold text-lg flex [&>li>a]:inline-block [&>li>a]:px-6 text-green-veri'>
           <li><a className='hover:text-blue-veri hover:text-xl duration-200' href="/">Instalaciones</a></li>
           <li><a className='hover:text-blue-veri hover:text-xl duration-200' href="/1">Acerca de</a></li>
@@ -15,7 +15,7 @@ const Header = () => {
       </nav>
       <nav className='flex flex-grow basis-0 justify-end mx-4'>
         <ul className='flex'>
-          <li><a className='' href="/"><img src="./img/whats.webp" alt="" /></a></li>
+          <li><a href="/"><img src="./img/whats.webp" alt="" /></a></li>
           <li className='ml-4'><a  href="/"><img src="./img/email.webp" alt="" /></a></li>
         </ul>
       </nav>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Main = () => {
   return (
     <main className='mt-[80px] w-full relative text-white -z-40'>
-      <div className='fixed'>
-        <img className='static -z-40 w-full' src="./img/panel.webp" alt="" />
-        <div className=' w-[60%] mx-auto absolute'>
-          <h1 className='font-montserrat font-bold text-4xl'>Una mejor manera de suministrarle energía a su hogar, empresa o negocio</h1>
+      <div className='fixed h-full'>
+        <img className='w-full absolute -z-50' src="./img/panel.webp" alt="" />
+        <div className='ml-[5%] mt-[10%] mx-auto'>
+          <h2 className='font-montserrat font-bold text-[45px]'>Una mejor manera de suministrarle energía a su hogar, empresa o negocio</h2>
           <p className='text-lg pb-8'>Nosotros nos encargamos de la instalación fotovoltaica con estándares muy altos de calidad. 
           Somos el único proveedor que usted va a requerir, desde que pide la instalación de los paneles 
           solares hasta que están inteconectados con CFE.
@@ -14,6 +14,15 @@ const Main = () => {
           <a className='bg-white/10 rounded-full border-2 p-2 hover:bg-white hover:text-black duration-200' href="/">Pedir una cotización</a>
         </div>
       </div>
+      <section id='arrays'>
+        <h1>
+          Instalaciones
+        </h1>
+        <div className='flex'>
+          <div className='flex'>1</div>
+          <div className='flex'>2</div>
+        </div>
+      </section>
     </main>
   )
 }
