b87bb584413ee88915581f9d29b6f03437130456
OswaldIR <os.ir.8897@gmail.com>
2024-01-16 18:21:13 -0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
